77d23659bc02a03634c57516bc7d7e8027995dba
Phil10020 <69660543+Phil10020@users.noreply.github.com>
2023-08-24 01:19:05 +0000

    Deploying to gh-pages from @ Phil10020/front@f5da1175d42caad50eae995515c47b25a94ee21b 🚀

---
+++ assets/VContainer-8e8eb8df.js
@@ -0,0 +1,1 @@
+import{p as n,m as o,a as r,b as l,o as c,u as i,c as u}from"./index-4d7e7858.js";const m=n({fluid:{type:Boolean,default:!1},...o(),...r()},"VContainer"),d=l()({name:"VContainer",props:m(),setup(e,a){let{slots:s}=a;const{rtlClasses:t}=c();return i(()=>u(e.tag,{class:["v-container",{"v-container--fluid":e.fluid},t.value,e.class],style:e.style},s)),{}}});export{d as V};
